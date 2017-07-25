fbe4555ca5735bbb419bb8e2c8c3fb00eafda854
Hassan Latif <hassan.latif@gmail.com>
2017-07-25 20:27:33 +0300

    WIP

--- hajj-mobile/src/app/app.component.ts
+++ hajj-mobile/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Alarms } from './interfaces/alarms';
 import { AlarmsService } from './services/alarms.service';
 import { Component, OnInit, Output } from '@angular/core';
+import * as _ from "lodash";
+
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,8 @@ import { Component, OnInit, Output } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Hajj Mobile dashboard';
   alarms: Alarms;
+  alarmsTrend: any;
+
 
   constructor(private alarmsService: AlarmsService) {
 
@@ -21,15 +25,54 @@ export class AppComponent implements OnInit {
   
   getAlarms() {
      this.alarmsService.getAlarms().subscribe(
-       (alarms: Alarms) => {
-        console.log(this.alarms);
-        this.alarms = alarms
-        console.log(this.alarms);
+       (newAlarms: Alarms) => {
+
+        if (newAlarms && this.alarms) {
+          this.getAlarmsTrend(newAlarms, this.alarms)         
+        }
+
+        this.alarms = newAlarms
+ 
         },
        (error) => console.log("Error fetching alarms:", error)
      );
 
   }
+
+	getAlarmsTrend(nAlarms, oAlarms) {	
+			var alarmsCount = {};
+
+			var oldAlarms = null;
+			var newAlarms = null;
+
+			if (!_.isEqual({}, oAlarms)) {
+
+				oldAlarms = {...oAlarms}; //deep copy
+				newAlarms = {...nAlarms};
+        
+        // console.log(newAlarms, oldAlarms)
+        
+        delete oldAlarms.Utilization;
+				delete oldAlarms.TechStats;
+				delete newAlarms.Utilization;
+				delete newAlarms.TechStats;			
+			}
+
+			for (let alarmType in newAlarms) {
+				for (let key in newAlarms[alarmType]) {
+					var alarmKey = alarmType + "_" + key;
+					var alarmTypeCount = 0;
+					if (!_.isEqual({}, oAlarms)) {
+						alarmTypeCount = (newAlarms[alarmType][key] - oldAlarms[alarmType][key]);
+					}
+					alarmsCount[alarmKey] = alarmTypeCount;
+
+				}
+			}
+
+			console.log(alarmsCount);
+			this.alarmsTrend = alarmsCount;
+		}  
   
 
 
